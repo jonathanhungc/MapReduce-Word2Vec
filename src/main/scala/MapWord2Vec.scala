3b965167d96424231c437966234f4ba12a52bd4f
Jonathan <jonathanhung2001@gmail.com>
2024-10-01 16:50:17 -0500

    Testing Map Reducer. Map works, Reducer not being called.

--- src/main/scala/MapWord2Vec.scala
+++ src/main/scala/MapWord2Vec.scala
@@ -2,6 +2,9 @@ import org.apache.hadoop.fs.Path
 import org.apache.hadoop.conf._
 import org.apache.hadoop.io._
 import org.apache.hadoop.mapreduce._
+import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.mapreduce.Mapper
+
 
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
@@ -34,8 +37,8 @@ class MapWord2Vec extends Mapper[LongWritable, Text, Text, ArrayWritable] {
   override def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, ArrayWritable]#Context): Unit = {
     val line: String = value.toString.toLowerCase
     val words = line.split("\\W+")
-    println("Mappppppp start")
-    sentences_list += line// adding line sentence to set for later training
+    // log.info("Mappppppp start")
+    sentences_list += line // adding line sentence to set for later training
     words.foreach(word =>
       words_tokens_set += (word + "," + enc.encode(word).get(0).toString)) // create keys (word,token) for later
 
@@ -45,7 +48,7 @@ class MapWord2Vec extends Mapper[LongWritable, Text, Text, ArrayWritable] {
     //    encoded.foreach { token =>
     //      context.write(new Text(token.get(0).toString), new IntWritable(1))
     //    }
-    println("Mappppppp end")
+    // log.info("Mappppppp end")
 
   }
 
@@ -60,9 +63,9 @@ class MapWord2Vec extends Mapper[LongWritable, Text, Text, ArrayWritable] {
     t.setTokenPreProcessor(new CommonPreprocessor)
     log.info("Building model....")
     val vec = new Word2Vec.Builder()
-      .minWordFrequency(5)
+      .minWordFrequency(1)
       .iterations(1)
-      .layerSize(100)
+      .layerSize(5)
       .seed(42)
       .windowSize(5)
       .iterate(iter)
@@ -72,22 +75,47 @@ class MapWord2Vec extends Mapper[LongWritable, Text, Text, ArrayWritable] {
     log.info("Fitting Word2Vec model....")
     vec.fit()
 
-    // println(words_tokens_set)
 
-    words_tokens_set.foreach{word =>
-      log.info("Working on word: " + word)
-      val vector = getVector(vec.getWordVector(word.split(",")(0)))
-//      val vectorString = vector.get().map(_.toString).mkString("[", ", ", "]")
-//      println(vectorString)
-      context.write(new Text(word), vector)
+    //log.info("Set of words and tokens: " + words_tokens_set.mkString(", "))
+    words_tokens_set.foreach { word =>
+      try {
+        log.info("Working on word: " + word)
+
+        val vectorArr: Array[Double] = vec.getWordVector(word.split(",")(0))    // get vector for word
+
+        if (vectorArr == null) {
+          log.warn(s"No vector found for word: ${word.split(",")(0)}")
+        } else {
+
+          // log.info(vectorArr.mkString("Array(", ", ", ")"))   // print vector for debugging
+
+          try {
+            // create Array of DoubleWritable
+            val doubleWritableArray: Array[Writable] = vectorArr.map(value => new DoubleWritable(value))
+            log.info("Successfully created doubleWritableArray")
+
+            // create ArrayWritable
+            val vector = new ArrayWritable(classOf[DoubleWritable], doubleWritableArray)
+            log.info("Successfully created ArrayWritable")
+            log.info(s"Key: ${word} Vector: ${vector.toString}")
+            // write to context
+            context.write(new Text(word), vector)
+            log.info(s"Successfully wrote word: $word and its vector to context")
+          } catch {
+            case e: Exception => log.error(s"Error in array or writing process for word: $word", e)
+          }
+        }
+      } catch {
+        case e: Exception => log.error(s"Error processing word: $word", e)
+      }
+      log.info("Cleannnnnnnn end")
     }
-    log.info("Cleannnnnnnn end")
-  }
 
-  // function to use array of doubles and write an ArrayWritable of DoubleWritable
-  private def getVector(vector: Array[Double]): ArrayWritable = {
+    // function to use array of doubles and write an ArrayWritable of DoubleWritable
+    def getVector(vector: Array[Double]): ArrayWritable = {
     val doubleWritableArray: Array[Writable] = vector.map(value => new DoubleWritable(value))
     new ArrayWritable(classOf[DoubleWritable], doubleWritableArray)
+    }
   }
 }
 
@@ -96,34 +124,38 @@ class ReduceWord2Vec extends Reducer[Text, ArrayWritable, Text, Text] {
   private val log = LoggerFactory.getLogger(classOf[ReduceWord2Vec])
 
   override def reduce(key: Text, values: java.lang.Iterable[ArrayWritable], context: Reducer[Text, ArrayWritable, Text, Text]#Context): Unit = {
+    log.info("Redddddddd start")
+    log.info(s"Reducing key: $key")
+    val arrayLength = 100
+    val averageValues = Array.fill(arrayLength)(new DoubleWritable(0.0))
+    var count = 0
 
-      log.info("Redddddddd start")
-      val arrayLength = 100
-      val averageValues = Array.fill(arrayLength)(new DoubleWritable(0.0))
-      var count = 0
+    values.asScala.foreach { arrayWritable =>
+      // get array from values, and cast to array of DoubleWritable
+      val doubleArray = arrayWritable.get().map(_.asInstanceOf[DoubleWritable].get())
 
-      values.asScala.foreach { arrayWritable =>
-        // get array from values, and cast to array of DoubleWritable
-        val doubleArray = arrayWritable.get().map(_.asInstanceOf[DoubleWritable].get())
-
-        // add each value to average vector
-        doubleArray.zipWithIndex.foreach { case (number, index) =>
-          val updatedValue = averageValues(index).get() + number // add values from each vector
-          averageValues(index).set(updatedValue) // update the value
-        }
-        count += 1
+      // add each value to average vector
+      doubleArray.zipWithIndex.foreach { case (number, index) =>
+        val updatedValue = averageValues(index).get() + number // add values from each vector
+        averageValues(index).set(updatedValue) // update the value
       }
+      count += 1
+    }
 
-      // calculate average for each number in average vector
-      averageValues.foreach { vectorNumber =>
-        vectorNumber.set(vectorNumber.get() / count)
-      }
+    // calculate average for each number in average vector
+    averageValues.foreach { vectorNumber =>
+      vectorNumber.set(vectorNumber.get() / count)
+    }
 
-      // write count of vectors and average vector to key
-      val averageString = averageValues.map(_.get().toString).mkString(" ")   // make string of average vector
-      context.write(key, new Text(count.toString + "," + averageString))
+    // write count of vectors and average vector to key
+    val averageString = averageValues.map(_.get().toString).mkString(" ")   // make string of average vector
+    context.write(key, new Text(count.toString + "," + averageString))
 
-      log.info("Redddddddd start")
+    //      val doubleWritableArray: Array[Writable] = averageValues.map(value => value)
+    //      val resultArrayWritable = new ArrayWritable(classOf[DoubleWritable], doubleWritableArray)
+    //      context.write(new Text("," + key + "," + count.toString), resultArrayWritable)
+
+    log.info("Redddddddd start")
   }
 }
 
@@ -134,9 +166,9 @@ object Word2VecDriver {
     val conf = new Configuration()
 
     conf.set("mapreduce.output.textoutputformat.separator", ",")
-    conf.set("fs.defaultFS", "local")
-    conf.set("mapreduce.job.maps", "1")
-    conf.set("mapreduce.job.reduces", "1")
+//    conf.set("fs.defaultFS", "local")
+//    conf.set("mapreduce.job.maps", "1")
+//    conf.set("mapreduce.job.reduces", "1")
 
     val job = Job.getInstance(conf, "Word2Vec Encoding Job")
 
@@ -146,6 +178,10 @@ object Word2VecDriver {
     // Set Mapper and Reducer classes
     job.setMapperClass(classOf[MapWord2Vec])
     job.setReducerClass(classOf[ReduceWord2Vec])
+
+    // Set mapper key and value class
+    job.setMapOutputKeyClass(classOf[Text])
+    job.setMapOutputValueClass(classOf[ArrayWritable])
 
     // Set output key and value types
     job.setOutputKeyClass(classOf[Text])
